abf9af7b7887935d62fca92fe0472ce7d3dec3f3
John Berlin Leonor <johnberlinleonor@yahoo.com>
2023-12-12 08:10:43 +0800

    Modified Asynchronous Database Operation Functionality
    Converted the Executor Service function with a Coroutine function to comply with Kotlin standards

--- Handsy/app/src/main/java/com/ruralnative/handsy/data/RoomDatabase.kt
+++ Handsy/app/src/main/java/com/ruralnative/handsy/data/RoomDatabase.kt
@@ -21,10 +21,6 @@ abstract class Database : RoomDatabase() {
         @Volatile
         private var INSTANCE: Database? = null
 
-        private const val NUMBER_OF_THREADS = 4
-
-        val databaseWriteExecutor: ExecutorService = Executor.newFixedThreadPool(NUMBER_OF_THREADS)
-
         fun getDatabase(application: Application): Database {
             if (INSTANCE == null) {
                 synchronized(Database::class) {
@@ -42,9 +38,13 @@ abstract class Database : RoomDatabase() {
         }
     }
 
+    fun getCoroutine(): CoroutineScope {
+        return CoroutineScope(Dispatchers.IO)
+    }
+
     abstract fun userDao(): userDao
-    abstract fun alphabetLessonsDao: alphabetLessonsDao
-    abstract fun phrasesLessonsDao: phrasesLessonsDao
+    abstract fun alphabetLessonsDao(): alphabetLessonsDao
+    abstract fun phrasesLessonsDao(): phrasesLessonsDao
 
     fun getDatabaseWriteExecutor(): ExecutorService {
         return databaseWriteExecutor
