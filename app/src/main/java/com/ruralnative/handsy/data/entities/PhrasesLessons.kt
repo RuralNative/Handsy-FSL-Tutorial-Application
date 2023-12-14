84a2fbce6099eca9b5ade487a64ce4127cb36f6f
John Berlin Leonor <johnberlinleonor@yahoo.com>
2023-12-14 13:50:15 +0800

    Create PhrasesLessons Entity
    Created the Entity for database table PhrasesLessons for Room Database use

---
+++ app/src/main/java/com/ruralnative/handsy/data/entities/PhrasesLessons.kt
@@ -0,0 +1,13 @@
+package com.ruralnative.handsy.data.entities
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "phrases_lessons")
+data class PhrasesLessons (
+    @PrimaryKey(autoGenerate = true) val id: Int,
+    @ColumnInfo(name = "lesson_name") val lessonName: String?,
+    @ColumnInfo(name = "lesson_description") val lessonDescription: String?,
+    @ColumnInfo(name = "lesson_media_file") val lessonMediaFile: String?
+)
