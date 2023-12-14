89c10da29a3adaf4e442bb91d58fb0060f375a21
John Berlin Leonor <johnberlinleonor@yahoo.com>
2023-12-14 13:46:58 +0800

    Create AlphabetLessons Entity
    Created the Entity for database table AlphabetLessons for Room Database use

--- app/src/main/java/com/ruralnative/handsy/data/entities/User.kt
+++ app/src/main/java/com/ruralnative/handsy/data/entities/User.kt
@@ -6,7 +6,6 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "user")
 data class User(
-    //id, user_name, is_new_user, progression_level
     @PrimaryKey(autoGenerate = true) val id: Int,
     @ColumnInfo(name = "user_name") val userName: String?,
     @ColumnInfo(name = "is_new_user") val isNewUser: Int,
