f0b97d45a022a13c3d6c82de6406db1677255247
John Berlin Leonor <johnberlinleonor@yahoo.com>
2023-12-14 16:33:27 +0800

    Provide Async Func for Alphabet Lesson DAO
    Added Coroutine functionality that allows one shot queries of the DAO to be performed asynchronously by the use of the 'suspend' keyword

--- app/src/main/java/com/ruralnative/handsy/data/dao/AlphabetLessonDao.kt
+++ app/src/main/java/com/ruralnative/handsy/data/dao/AlphabetLessonDao.kt
@@ -11,27 +11,27 @@ import com.ruralnative.handsy.data.entities.AlphabetLesson
 @Dao
 interface AlphabetLessonDao {
 
-    @Query("SELECT * from alphabet_lessons WHERE id = :lessonID")
-    fun selectLessonById(lessonID: Int): AlphabetLesson
-
     @Query("SELECT * from alphabet_lessons")
     fun selectAllLessons(): List<AlphabetLesson>
 
+    @Query("SELECT * from alphabet_lessons WHERE id = :lessonID")
+    suspend fun selectLessonById(lessonID: Int): AlphabetLesson
+
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insertLesson(lesson: AlphabetLesson)
+    suspend fun insertLesson(lesson: AlphabetLesson)
 
     @Update
-    fun updateLesson(lesson: AlphabetLesson)
+    suspend fun updateLesson(lesson: AlphabetLesson)
 
     @Query("UPDATE alphabet_lessons SET lesson_name = :lessonName WHERE id = :lessonID")
-    fun updateLessonName(lessonName: String?, lessonID: String)
+    suspend fun updateLessonName(lessonName: String?, lessonID: String)
 
     @Query("UPDATE alphabet_lessons SET lesson_description = :lessonDescription WHERE id = :lessonID")
-    fun updateLessonDescription(lessonDescription: String?, lessonID: String)
+    suspend fun updateLessonDescription(lessonDescription: String?, lessonID: String)
 
     @Query("UPDATE alphabet_lessons SET lesson_media_file = :lessonMediaFile WHERE id = :lessonID")
-    fun updateLessonMediaFile(lessonMediaFile: String?, lessonID: String)
+    suspend fun updateLessonMediaFile(lessonMediaFile: String?, lessonID: String)
 
     @Delete
-    fun deleteLesson(lesson: AlphabetLesson)
+    suspend fun deleteLesson(lesson: AlphabetLesson)
 }
