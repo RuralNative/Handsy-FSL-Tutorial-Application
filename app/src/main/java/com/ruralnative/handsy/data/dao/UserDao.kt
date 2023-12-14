7a093ad69ced7fd9a58a03e686c7cec4851d4473
John Berlin Leonor <johnberlinleonor@yahoo.com>
2023-12-14 14:39:41 +0800

    Finalized the User DAO
    Created the query-level data access methods of the User's DAO

--- app/src/main/java/com/ruralnative/handsy/data/dao/UserDao.kt
+++ app/src/main/java/com/ruralnative/handsy/data/dao/UserDao.kt
@@ -20,6 +20,15 @@ interface UserDao {
     @Update
     fun updateUser(user: User)
 
+    @Query("UPDATE user SET user_name = :userName WHERE id = :userID")
+    fun updateUserName(userName: String?, userID: Int)
+
+    @Query("UPDATE user SET is_new_user = :boolValue WHERE id = :userID")
+    fun updateUserStatus(boolValue: Int, userID: Int)
+
+    @Query("UPDATE user SET progression_level = :userLevel WHERE id = :userID")
+    fun updateUserProgressionLevel(userLevel: Int, userID: Int)
+
     @Delete
     fun deleteUser(user: User)
 }
