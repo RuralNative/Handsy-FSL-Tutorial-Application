9f693c617bc2d8c6dde206503e57faf9a1e21daa
John Berlin Leonor <johnberlinleonor@yahoo.com>
2023-12-14 13:42:21 +0800

    Create User Entity
    Created the entity of User table with specifications of its table columns

--- app/src/main/java/com/ruralnative/handsy/data/entities/User.kt
+++ app/src/main/java/com/ruralnative/handsy/data/entities/User.kt
@@ -1,10 +1,14 @@
 package com.ruralnative.handsy.data.entities
 
-import a ndroidx.room.Entity
+import androidx.room.ColumnInfo
+import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = 'user')
+@Entity(tableName = "user")
 data class User(
     //id, user_name, is_new_user, progression_level
-    @PrimaryKey val
+    @PrimaryKey(autoGenerate = true) val id: Int,
+    @ColumnInfo(name = "user_name") val userName: String?,
+    @ColumnInfo(name = "is_new_user") val isNewUser: Int,
+    @ColumnInfo(name = "progression_level") val progressionLevel: Int
 )
