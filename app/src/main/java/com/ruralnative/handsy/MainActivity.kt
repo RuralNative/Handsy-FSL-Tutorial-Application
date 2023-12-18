cbafcc43a74cbe3300fc17ab343c97db2f546b42
John Berlin Leonor <johnberlinleonor@yahoo.com>
2023-12-18 17:36:33 +0800

    Modify Main Acitvity Class
    Provide outline elements and modifications outside of styling for the use of the Welcome Screen

--- app/src/main/java/com/ruralnative/handsy/MainActivity.kt
+++ app/src/main/java/com/ruralnative/handsy/MainActivity.kt
@@ -3,11 +3,15 @@ package com.ruralnative.handsy
 import  android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.ruralnative.handsy.ui.theme.HandsyTheme
@@ -22,7 +26,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    ShowAppName()
                 }
             }
         }
@@ -30,17 +34,30 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ShowAppName(modifier: Modifier = Modifier) {
+    Column() {
+        Text(
+            text = "HANDSY",
+            modifier = modifier
+        )
+        Text(
+            text = "FSL Application",
+            modifier = modifier
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    HandsyTheme {
-        Greeting("Android")
+fun WelcomeScreen() {
+    Row(
+        modifier = Modifier.fillMaxSize(),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        HandsyTheme {
+            ShowAppName()
+        }
     }
+
 }
