37dbcdb697dd2c68a78d55a785c533c5faa3b1ea
John Berlin Leonor <johnberlinleonor@yahoo.com>
2023-12-18 16:54:45 +0800

    Modified Compose Typography
    Used customized imported fonts for use for the Compose Theme of the application

--- app/src/main/java/com/ruralnative/handsy/ui/theme/Type.kt
+++ app/src/main/java/com/ruralnative/handsy/ui/theme/Type.kt
@@ -2,35 +2,42 @@ package com.ruralnative.handsy.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.ruralnative.handsy.R
 
-val 
+val nunitoFontFamily = FontFamily(
+    Font(R.font.feather_bold, weight = FontWeight.ExtraBold),
+    Font(R.font.nunito_black, weight = FontWeight.Black),
+    Font(R.font.nunito_bold, weight = FontWeight.Bold),
+    Font(R.font.nunito_regular, weight = FontWeight.Normal),
+    Font(R.font.nunito_light, weight = FontWeight.Light)
+)
+
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = nunitoFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+    ),
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+        fontFamily = nunitoFontFamily,
+        fontWeight = FontWeight.ExtraBold,
         fontSize = 22.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
+        fontFamily = nunitoFontFamily,
+        fontWeight = FontWeight.Light,
         fontSize = 11.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
     )
-    */
 )
