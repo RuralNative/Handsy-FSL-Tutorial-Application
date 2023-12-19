f0a18703b6b8b9a5831f5d7a1dd1412b531c6551
John Berlin Leonor <johnberlinleonor@yahoo.com>
2023-12-19 15:55:28 +0800

    Modified Main Activity Class
    Relocated the code of Main Activity to the main directory for the UI Layer of the application for its role to be changed as the main entry point of the application

--- app/src/main/java/com/ruralnative/handsy/ui/app/MainActivity.kt
+++ app/src/main/java/com/ruralnative/handsy/ui/app/MainActivity.kt
@@ -1,21 +1,24 @@
-package com.ruralnative.handsy
+package com.ruralnative.handsy.ui.app
 
 import  android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
+import com.ruralnative.handsy.R
 import com.ruralnative.handsy.ui.theme.HandsyTheme
 
 class MainActivity : ComponentActivity() {
@@ -59,20 +62,35 @@ fun ShowAppName(modifier: Modifier = Modifier) {
 
 @Composable
 fun CreateDisplayIcon() {
-    Row(
-        modifier = Modifier.fillMaxSize(),
-        horizontalArrangement = Arrangement.Center,
-        verticalAlignment = Alignment.CenterVertically
+    ConstraintLayout (
+        modifier = Modifier.fillMaxSize()
     ) {
-        HandsyTheme {
-            ShowMascot()
-            ShowAppName()
+        val container = createRef()
+
+        Column (
+            modifier = Modifier.constrainAs(container) {
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
+                top.linkTo(parent.top)
+                bottom.linkTo(parent.bottom)
+            }
+        ) {
+            HandsyTheme {
+                ShowMascot()
+                ShowAppName()
+            }
         }
     }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun WelcomeScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+
+    }
     CreateDisplayIcon()
 }
