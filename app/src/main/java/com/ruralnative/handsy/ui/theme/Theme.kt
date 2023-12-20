e867083475e26b93733ac017820e34289f941474
John Berlin Leonor <johnberlinleonor@yahoo.com>
2023-12-20 09:35:51 +0800

    Update Theme File
    Updated the Theme file to adapt with the changes of our application's Color Scheme

--- app/src/main/java/com/ruralnative/handsy/ui/theme/Theme.kt
+++ app/src/main/java/com/ruralnative/handsy/ui/theme/Theme.kt
@@ -10,36 +10,21 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80,
-    background = backgroundColor,
-    surface = backgroundColor,
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = backgroundColor,
-    onSurface = backgroundColor
+    primary = BackgroundColor,
+    secondary = RegularColor,
+    tertiary = HighlightColor
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40,
-    background = backgroundColor,
-    surface = backgroundColor,
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = backgroundColor,
-    onSurface = backgroundColor
+    primary = DarkBackgroundColor,
+    secondary = RegularColor,
+    tertiary = HighlightColor
 )
 
 @Composable
