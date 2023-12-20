3d06b5fb19a6da5804ae01382a1da352caafb1f5
John Berlin Leonor <johnberlinleonor@yahoo.com>
2023-12-20 09:35:21 +0800

    Modified Color scheme
    Modified the values and variables used for the application color scheme from Purple to Green-based

--- app/src/main/java/com/ruralnative/handsy/ui/theme/Color.kt
+++ app/src/main/java/com/ruralnative/handsy/ui/theme/Color.kt
@@ -2,11 +2,7 @@ package com.ruralnative.handsy.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val backgroundColor = Color(0xFF21A316)
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val BackgroundColor = Color(0xFF21A316)
+val DarkBackgroundColor = Color(0xFF003300)
+val HighlightColor = Color(0xFF52B4F1)
+val RegularColor = Color(0xFFF6EDD9)
