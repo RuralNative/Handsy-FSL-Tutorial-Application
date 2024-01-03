f244930e5c51bfa3ac7348cbdae0f334f90105ee
John Berlin Leonor <johnberlinleonor@yahoo.com>
2024-01-03 10:31:35 +0800

    Add Route Parameter for Screen Class
    Added a String parameter named ROUTE that provides the unique identity for each screen

--- app/src/main/java/com/ruralnative/handsy/Screen.kt
+++ app/src/main/java/com/ruralnative/handsy/Screen.kt
@@ -3,8 +3,8 @@ package com.ruralnative.handsy
 /*
 This sealed class Screen represents the different Composable screens in the application, restricting the class hierarchy to what is defined here, allowing handling of navigation results to be easier.
  */
-sealed class Screen {
-    object LoadingScreen: Screen()
-    object UserIntroScreen: Screen()
-    object AuthorMessageScreen: Screen()
+sealed class Screen(val route: String) {
+    object LoadingScreen: Screen(route = "loading_screen")
+    object UserIntroScreen: Screen(route = "user_intro_screen")
+    object AuthorMessageScreen: Screen(route = "author_message_screen")
 }
