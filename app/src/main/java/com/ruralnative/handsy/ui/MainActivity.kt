0501782d4d95564461f0773ef86de9b47b4b8ee3
John Berlin Leonor <johnberlinleonor@yahoo.com>
2024-01-03 11:14:51 +0800

    Create Variable for Navigation Host/Controller
    Created an instantiation of Navigation Host Controller for the Main Activity

--- app/src/main/java/com/ruralnative/handsy/ui/MainActivity.kt
+++ app/src/main/java/com/ruralnative/handsy/ui/MainActivity.kt
@@ -3,40 +3,19 @@ package com.ruralnative.handsy.ui
 import  android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.constraintlayout.compose.ConstraintLayout
-import com.ruralnative.handsy.R
-import com.ruralnative.handsy.ui.theme.BackgroundColor
-import com.ruralnative.handsy.ui.theme.DarkBackgroundColor
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import com.ruralnative.handsy.ui.theme.HandsyTheme
-import com.ruralnative.handsy.ui.theme.NunitoFontFamily
-import com.ruralnative.handsy.ui.theme.RegularColor
 
 class MainActivity : ComponentActivity() {
+
+    lateinit var navController: NavHostController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             HandsyTheme {
-                //TODO
+                navController = rememberNavController()
             }
         }
     }
