39239a69c42a318d2a32b09839183e188860431a
John Berlin Leonor <johnberlinleonor@yahoo.com>
2024-01-03 12:59:25 +0800

    Added Navigation Controller for Main Activity
    Added the code for NavController for use of the application

--- app/src/main/java/com/ruralnative/handsy/ui/MainActivity.kt
+++ app/src/main/java/com/ruralnative/handsy/ui/MainActivity.kt
@@ -16,6 +16,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             HandsyTheme {
                 navController = rememberNavController()
+                NavGraph(navController = navController)
             }
         }
     }
