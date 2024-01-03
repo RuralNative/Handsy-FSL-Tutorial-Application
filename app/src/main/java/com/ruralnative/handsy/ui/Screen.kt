392a659f35a3b8961cfaca0c448f148a0506a98f
John Berlin Leonor <johnberlinleonor@yahoo.com>
2024-01-03 12:50:26 +0800

    Update Screen.kt
    Added data keyword to better represent the role of the variables inside the Screen sealed class

--- app/src/main/java/com/ruralnative/handsy/ui/Screen.kt
+++ app/src/main/java/com/ruralnative/handsy/ui/Screen.kt
@@ -4,7 +4,7 @@ package com.ruralnative.handsy.ui
 This sealed class Screen represents the different Composable screens in the application, restricting the class hierarchy to what is defined here, allowing handling of navigation results to be easier.
  */
 sealed class Screen(val route: String) {
-    object LoadingScreen: Screen(route = "loading_screen")
-    object UserIntroScreen: Screen(route = "user_intro_screen")
-    object AuthorMessageScreen: Screen(route = "author_message_screen")
+    data object LoadingScreen: Screen(route = "loading_screen")
+    data object UserIntroScreen: Screen(route = "user_intro_screen")
+    data object AuthorMessageScreen: Screen(route = "author_message_screen")
 }
