dfa7449c48c33f77162dcad2391b4c995498570e
John Berlin Leonor <johnberlinleonor@yahoo.com>
2024-01-03 13:27:51 +0800

    Added Navigation Dependencies
    Added dependencies inside the Build Gradle file for use of the application the Navigation library provided by Jetpack Compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,12 +66,18 @@ dependencies {
     val composeBom = platform("androidx.compose:compose-bom:2023.10.01")
     implementation(composeBom)
     androidTestImplementation(composeBom)
-
-    implementation("androidx.navigation:navigation-compose:2.7.6")
-    implementation("androidx.activity:activity-compose:1.8.1")
+    implementation("androidx.activity:activity-compose:1.8.2")
     implementation("androidx.compose.runtime:runtime-livedata")
     implementation("androidx.constraintlayout:constraintlayout-compose:1.0.1")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    //Navigation
+    val navVersion = "2.7.6"
+    implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
+    implementation("androidx.navigation:navigation-ui-ktx:$navVersion")
+    implementation("androidx.navigation:navigation-dynamic-features-fragment:$navVersion")
+    implementation("androidx.navigation:navigation-compose:$navVersion")
+    androidTestImplementation("androidx.navigation:navigation-testing:$navVersion")
 
     // Material Design 3
     implementation("androidx.compose.material3:material3")
