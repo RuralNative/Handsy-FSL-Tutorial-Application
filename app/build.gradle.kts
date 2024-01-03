3c122f6ff7f74976f5afaeed8a32aa402ae9f50c
John Berlin Leonor <johnberlinleonor@yahoo.com>
2024-01-03 08:31:42 +0800

    Add Compose Navigation Dependency
    Added a dependency to use the Compose-compatible Navigation library for the use of the application.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,6 +66,8 @@ dependencies {
     val composeBom = platform("androidx.compose:compose-bom:2023.10.01")
     implementation(composeBom)
     androidTestImplementation(composeBom)
+
+    implementation("androidx.navigation:navigation-compose:2.7.6")
     implementation("androidx.activity:activity-compose:1.8.1")
     implementation("androidx.compose.runtime:runtime-livedata")
     implementation("androidx.constraintlayout:constraintlayout-compose:1.0.1")
