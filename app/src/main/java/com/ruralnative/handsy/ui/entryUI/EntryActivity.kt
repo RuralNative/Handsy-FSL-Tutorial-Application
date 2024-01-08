8b38f8b7af321e25d2c50af9bdd84c16b21a7179
John Berlin Leonor <johnberlinleonor@yahoo.com>
2024-01-08 16:05:22 +0800

    Added View Model for Entry Activity
    Added the initialization of the ViewModel for the Entry Activity to be utilized for navigating to different Activities

--- app/src/main/java/com/ruralnative/handsy/ui/entryUI/EntryActivity.kt
+++ app/src/main/java/com/ruralnative/handsy/ui/entryUI/EntryActivity.kt
@@ -7,11 +7,16 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.ViewModelProvider
 import com.ruralnative.handsy.ui.theme.HandsyTheme
 
 class EntryActivity : ComponentActivity() {
+    private lateinit var viewModel: EntryViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        viewModel = ViewModelProvider(this)[EntryViewModel::class.java]
+
         setContent {
             HandsyTheme {
                 Surface (
@@ -22,5 +27,18 @@ class EntryActivity : ComponentActivity() {
                 }
             }
         }
+
+        viewModel.checkUserCountAndNavigate(
+            navigateToInitial = navigateForNewUser(),
+            navigateToMain =  navigateForCurrentUser()
+        )
+    }
+
+    private fun navigateForNewUser(): () -> Unit {
+        TODO("No User Intro Classes")
+    }
+
+    private fun navigateForCurrentUser(): () -> Unit {
+        TODO("Not Main Screen Classes")
     }
 }
