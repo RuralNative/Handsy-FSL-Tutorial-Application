6506b5c1e349ab8a6a4d7e5a27305d9d64487970
John Berlin Leonor <johnberlinleonor@yahoo.com>
2024-01-08 15:57:01 +0800

    Add Logic for ViewModel
    Added the code to check Repository function and, depending on result, send a Flow<Boolean> object to ComponentActivity

--- app/src/main/java/com/ruralnative/handsy/ui/entryUI/EntryViewModel.kt
+++ app/src/main/java/com/ruralnative/handsy/ui/entryUI/EntryViewModel.kt
@@ -1,4 +1,31 @@
 package com.ruralnative.handsy.ui.entryUI
 
-class EntryViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.ruralnative.handsy.data.repository.UserRepository
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.single
+import kotlinx.coroutines.launch
+
+class EntryViewModel(private val repository: UserRepository): ViewModel() {
+    /*
+    TODO:
+        Check if there is an entry in Database.
+        If True, then Navigate to Main Screen
+        If False, then Navigate to Initial Screen
+     */
+    fun checkUserCountAndNavigate(
+        navigateToInitial: () -> Unit,
+        navigateToMain: () -> Unit
+    ) {
+        viewModelScope.launch {
+            delay(2000) // wait for 2 seconds
+            val isThereUser: Boolean = repository.isThereAUser().single()
+            if (isThereUser) {
+                navigateToMain()
+            } else {
+                navigateToInitial()
+            }
+        }
+    }
 }
