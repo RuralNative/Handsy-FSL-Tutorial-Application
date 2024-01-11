8da95e226a220d65aeac8462b0dab6526abe1eca
John Berlin Leonor <johnberlinleonor@yahoo.com>
2024-01-11 14:22:23 +0800

    Added Logging Feature
    Added logging functionality for the Activity for debugging/testing purposes of its Lifecycle phases

--- app/src/main/java/com/ruralnative/handsy/ui/MainActivity.kt
+++ app/src/main/java/com/ruralnative/handsy/ui/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.ruralnative.handsy.ui
 
 import  android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.ruralnative.handsy.ui.theme.HandsyTheme
+
+private const val TAG = "MainActivity"
 
 class MainActivity : ComponentActivity() {
 
@@ -13,9 +16,30 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "OnCreate() EXECUTED")
         setContent {
             val navController = rememberNavController()
             NavGraph(navController = navController)
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "OnStart() EXECUTED")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "OnStop() EXECUTED")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "OnResume() EXECUTED")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "OnPause() EXECUTED")
     }
 }
