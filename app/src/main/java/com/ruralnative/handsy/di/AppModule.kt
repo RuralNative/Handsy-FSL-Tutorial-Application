0e76277a214bca9d747bc0c03a2522a738c383f9
John Berlin Leonor <johnberlinleonor@yahoo.com>
2024-01-12 13:56:17 +0800

    Added DAO in App Module
    Added annotated methods for DAO instantiation inside the App Module

--- app/src/main/java/com/ruralnative/handsy/di/AppModule.kt
+++ app/src/main/java/com/ruralnative/handsy/di/AppModule.kt
@@ -3,6 +3,9 @@ package com.ruralnative.handsy.di
 import android.content.Context
 import androidx.room.Room
 import com.ruralnative.handsy.data.AppDatabase
+import com.ruralnative.handsy.data.dao.AlphabetLessonDao
+import com.ruralnative.handsy.data.dao.PhrasesLessonDao
+import com.ruralnative.handsy.data.dao.UserDao
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -28,6 +31,19 @@ object AppModule {
 
     @Provides
     @Singleton
-    fun provide
+    fun provideAlphabetLessonDao(
+        database: AppDatabase
+    ): AlphabetLessonDao = database.alphabetLessonsDao()
 
+    @Provides
+    @Singleton
+    fun providePhrasesLessonDao(
+        database: AppDatabase
+    ): PhrasesLessonDao = database.phrasesLessonDao()
+
+    @Provides
+    @Singleton
+    fun provideUserDao(
+        database: AppDatabase
+    ): UserDao = database.userDao()
 }
