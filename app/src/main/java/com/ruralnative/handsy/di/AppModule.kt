11499d59d8c8b0a963365a372102284a876d616d
John Berlin Leonor <johnberlinleonor@yahoo.com>
2024-01-12 13:59:32 +0800

    Added Repository Methods for App Module

--- app/src/main/java/com/ruralnative/handsy/di/AppModule.kt
+++ app/src/main/java/com/ruralnative/handsy/di/AppModule.kt
@@ -1,11 +1,15 @@
 package com.ruralnative.handsy.di
 
 import android.content.Context
+import android.widget.AlphabetIndexer
 import androidx.room.Room
 import com.ruralnative.handsy.data.AppDatabase
 import com.ruralnative.handsy.data.dao.AlphabetLessonDao
 import com.ruralnative.handsy.data.dao.PhrasesLessonDao
 import com.ruralnative.handsy.data.dao.UserDao
+import com.ruralnative.handsy.data.repository.AlphabetLessonRepository
+import com.ruralnative.handsy.data.repository.PhrasesLessonRepository
+import com.ruralnative.handsy.data.repository.UserRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -46,4 +50,22 @@ object AppModule {
     fun provideUserDao(
         database: AppDatabase
     ): UserDao = database.userDao()
+
+    @Provides
+    @Singleton
+    fun provideAlphabetLessonRepository(
+        dao: AlphabetLessonDao
+    ): AlphabetLessonRepository = AlphabetLessonRepository(dao)
+
+    @Provides
+    @Singleton
+    fun providePhrasesLessonRepository(
+        dao: PhrasesLessonDao
+    ): PhrasesLessonRepository = PhrasesLessonRepository(dao)
+
+    @Provides
+    @Singleton
+    fun provideUserRepository(
+        dao: UserDao
+    ): UserRepository = UserRepository(dao)
 }
