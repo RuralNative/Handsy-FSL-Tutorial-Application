435f500b6c07bc2db4696f0043ecf0e20328dc3e
John Berlin Leonor <johnberlinleonor@yahoo.com>
2024-01-15 21:53:56 +0800

    Modify DAO Methods for Suspend

--- app/src/main/java/com/ruralnative/handsy/data/dao/AlphabetLessonDao.kt
+++ app/src/main/java/com/ruralnative/handsy/data/dao/AlphabetLessonDao.kt
@@ -21,20 +21,20 @@ interface AlphabetLessonDao {
     fun selectLessonById(lessonID: Int): Flow<AlphabetLesson>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insertLesson(lesson: AlphabetLesson)
+    suspend fun insertLesson(lesson: AlphabetLesson)
 
     @Update
-    fun updateLesson(lesson: AlphabetLesson)
+    suspend fun updateLesson(lesson: AlphabetLesson)
 
     @Query("UPDATE alphabet_lessons SET lesson_name = :lessonName WHERE id = :lessonID")
-    fun updateLessonName(lessonName: String?, lessonID: Int)
+    suspend fun updateLessonName(lessonName: String?, lessonID: Int)
 
     @Query("UPDATE alphabet_lessons SET lesson_description = :lessonDescription WHERE id = :lessonID")
-    fun updateLessonDescription(lessonDescription: String?, lessonID: Int)
+    suspend fun updateLessonDescription(lessonDescription: String?, lessonID: Int)
 
     @Query("UPDATE alphabet_lessons SET lesson_media_file = :lessonMediaFile WHERE id = :lessonID")
-    fun updateLessonMediaFile(lessonMediaFile: String?, lessonID: Int)
+    suspend fun updateLessonMediaFile(lessonMediaFile: String?, lessonID: Int)
 
     @Delete
-    fun deleteLesson(lesson: AlphabetLesson)
+    suspend fun deleteLesson(lesson: AlphabetLesson)
 }
--- app/src/main/java/com/ruralnative/handsy/data/dao/UserDao.kt
+++ app/src/main/java/com/ruralnative/handsy/data/dao/UserDao.kt
@@ -14,29 +14,29 @@ import kotlinx.coroutines.flow.Flow
 interface UserDao {
 
     @Query("SELECT * from user")
-    fun selectAllUsers(): Flow<List<User>>
+    suspend fun selectAllUsers(): Flow<List<User>>
 
     @Query("SELECT * from user WHERE id = :userID")
-    fun selectUserById(userID: Int): Flow<User>
+    suspend fun selectUserById(userID: Int): Flow<User>
 
     @Query("SELECT COUNT(*) from user")
-    fun countUsers(): Flow<Int>
+    suspend fun countUsers(): Flow<Int>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insertUser(user: User)
+    suspend fun insertUser(user: User)
 
     @Update
-    fun updateUser(user: User)
+    suspend fun updateUser(user: User)
 
     @Query("UPDATE user SET user_name = :userName WHERE id = :userID")
-    fun updateUserName(userName: String?, userID: Int)
+    suspend fun updateUserName(userName: String?, userID: Int)
 
     @Query("UPDATE user SET is_new_user = :boolValue WHERE id = :userID")
-    fun updateUserStatus(boolValue: Int, userID: Int)
+    suspend fun updateUserStatus(boolValue: Int, userID: Int)
 
     @Query("UPDATE user SET progression_level = :userLevel WHERE id = :userID")
-    fun updateUserProgressionLevel(userLevel: Int, userID: Int)
+    suspend fun updateUserProgressionLevel(userLevel: Int, userID: Int)
 
     @Delete
-    fun deleteUser(user: User)
+    suspend fun deleteUser(user: User)
 }
