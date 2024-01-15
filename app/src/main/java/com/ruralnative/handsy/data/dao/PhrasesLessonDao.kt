346a69a36ec90fcf36299c5111d190d99330a588
John Berlin Leonor <johnberlinleonor@yahoo.com>
2024-01-15 21:54:52 +0800

    Update Phrases Lesson DAO for Suspend

--- app/src/main/java/com/ruralnative/handsy/data/dao/PhrasesLessonDao.kt
+++ app/src/main/java/com/ruralnative/handsy/data/dao/PhrasesLessonDao.kt
@@ -20,20 +20,20 @@ interface PhrasesLessonDao {
     fun selectLessonById(lessonID: Int): Flow<PhrasesLesson>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insertLesson(lesson: PhrasesLesson)
+    suspend fun insertLesson(lesson: PhrasesLesson)
 
     @Update
-    fun updateLesson(lesson: PhrasesLesson)
+    suspend fun updateLesson(lesson: PhrasesLesson)
 
     @Query("UPDATE phrases_lessons SET lesson_name = :lessonName WHERE id = :lessonID")
-    fun updateLessonName(lessonName: String?, lessonID: Int)
+    suspend fun updateLessonName(lessonName: String?, lessonID: Int)
 
     @Query("UPDATE phrases_lessons SET lesson_description = :lessonDescription WHERE id = :lessonID")
-    fun updateLessonDescription(lessonDescription: String?, lessonID: Int)
+    suspend fun updateLessonDescription(lessonDescription: String?, lessonID: Int)
 
     @Query("UPDATE phrases_lessons SET lesson_media_file = :lessonMediaFile WHERE id = :lessonID")
-    fun updateLessonMediaFile(lessonMediaFile: String?, lessonID: Int)
+    suspend fun updateLessonMediaFile(lessonMediaFile: String?, lessonID: Int)
 
     @Delete
-    fun deleteLesson(lesson: PhrasesLesson)
+    suspend fun deleteLesson(lesson: PhrasesLesson)
 }
