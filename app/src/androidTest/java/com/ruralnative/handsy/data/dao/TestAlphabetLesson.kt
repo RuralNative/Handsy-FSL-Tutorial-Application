2ab25428662e90c417e5f9cf4fe972b0aa13952d
John Berlin Leonor <johnberlinleonor@yahoo.com>
2024-01-16 11:52:58 +0800

    Fix for Race Condition Bug for Test
    Used coroutine functionalities for fixing race condition bug of the deleteAndCheckDatabaseContent() function

--- app/src/androidTest/java/com/ruralnative/handsy/data/dao/TestAlphabetLesson.kt
+++ app/src/androidTest/java/com/ruralnative/handsy/data/dao/TestAlphabetLesson.kt
@@ -6,20 +6,16 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import com.ruralnative.handsy.data.AppDatabase
 import com.ruralnative.handsy.data.entities.AlphabetLesson
-import dagger.hilt.android.testing.HiltAndroidRule
-import dagger.hilt.android.testing.HiltAndroidTest
-import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.test.advanceUntilIdle
-import kotlinx.coroutines.test.runBlockingTest
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert.assertEquals
 import org.junit.Before
-import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import javax.inject.Inject
 
 @RunWith(AndroidJUnit4::class)
 class TestAlphabetLesson {
@@ -39,26 +35,21 @@ class TestAlphabetLesson {
     @Test
     fun insertAndRetrieveLesson() = runTest {
         println("TESTING: insertLesson() and selectLessonByID()")
-
         val lesson = AlphabetLesson(
             1,
             "Lesson 1",
             "Lesson Desc 1",
             "Lesson Media File 1"
         )
-
         dao.insertLesson(lesson)
-
         val retrievedLesson = dao.selectLessonById(lesson.id)
         assertEquals(lesson, retrievedLesson.firstOrNull())
-
         println("TESTED: insertLesson() and selectLessonByID()")
     }
 
     @Test
     fun updateLesson() = runTest {
         println("TESTING: updateLesson()")
-
         val lesson = AlphabetLesson(
             1,
             "Lesson 1",
@@ -66,7 +57,6 @@ class TestAlphabetLesson {
             "Lesson Media File 1"
         )
         dao.insertLesson(lesson)
-
         val updatedLesson = AlphabetLesson(
             1,
             "MOD Lesson 1",
@@ -74,7 +64,6 @@ class TestAlphabetLesson {
             "MOD Lesson Media File 1"
         )
         dao.updateLesson(updatedLesson)
-
         val retrievedUpdatedLesson = dao.selectLessonById(1)
         assertEquals(retrievedUpdatedLesson.firstOrNull(), updatedLesson)
     }
@@ -82,7 +71,6 @@ class TestAlphabetLesson {
     @Test
     fun updateLessonName() = runTest {
         println("TESTING: updateLessonName(), updateLessonDescription, updateLessonMediaFile")
-
         val lesson = AlphabetLesson(
             1,
             "Lesson 1",
@@ -90,11 +78,9 @@ class TestAlphabetLesson {
             "Lesson Media File 1"
         )
         dao.insertLesson(lesson)
-
         dao.updateLessonName("Lesson 2", 1)
         dao.updateLessonDescription("Lesson Desc 2", 1)
         dao.updateLessonMediaFile("Lesson Media File 2", 1)
-
         val comparedLesson = AlphabetLesson(
             1,
             "Lesson 2",
@@ -102,15 +88,12 @@ class TestAlphabetLesson {
             "Lesson Media File 2"
         )
         val updatedLesson = dao.selectLessonById(1).firstOrNull()
-
         assertEquals(updatedLesson, comparedLesson)
     }
 
-    @OptIn(ExperimentalCoroutinesApi::class)
     @Test
     fun deleteAndCheckDatabaseContent() = runTest {
         println("TESTING: deleteLesson(), selectAllLessons()")
-
         val lessonOne = AlphabetLesson(
             1,
             "Lesson 1",
@@ -123,15 +106,30 @@ class TestAlphabetLesson {
             "Lesson Desc 2",
             "Lesson Media 2"
         )
-
+        //Test Before Delete
         var numberOfUsersUndeleted = 0
-
-        dao.insertLesson(lessonOne)
-        dao.insertLesson(lessonTwo)
-        val flowListOfUndeleted = dao.selectAllLessons()
-        flowListOfUndeleted.collect { users ->
-            numberOfUsersUndeleted = users.size
+        val insertLessonJob : Job = launch {
+            dao.insertLesson(lessonOne)
+            dao.insertLesson(lessonTwo)
+            delay(1000L)
+            val usersListFlow = dao.selectAllLessons()
+            usersListFlow.collectLatest { users ->
+                numberOfUsersUndeleted = users.size
+            }
         }
+        insertLessonJob.join()
         assertEquals(2, numberOfUsersUndeleted)
+        //Test After Delete
+        var numberOfUsersAfterDelete = 0
+        val deleteLessonJob : Job = launch {
+            dao.deleteLesson(lessonOne)
+            delay(1000L)
+            val usersListFlow = dao.selectAllLessons()
+            usersListFlow.collectLatest { users ->
+                numberOfUsersAfterDelete = users.size
+            }
+        }
+        deleteLessonJob.join()
+        assertEquals(1, numberOfUsersAfterDelete)
     }
 }
