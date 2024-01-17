72ff4a909e54548f1e06fc8365b1ad9137e6848f
John Berlin Leonor <johnberlinleonor@yahoo.com>
2024-01-17 09:28:04 +0800

    Create Hilt Test Module

---
+++ app/src/androidTest/java/com/ruralnative/handsy/data/di/FakeAppModule.kt
@@ -0,0 +1,60 @@
+package com.ruralnative.handsy.data.di
+
+import android.content.Context
+import com.ruralnative.handsy.data.AppDatabase
+import com.ruralnative.handsy.data.dao.AlphabetLessonDao
+import com.ruralnative.handsy.data.dao.PhrasesLessonDao
+import com.ruralnative.handsy.data.dao.UserDao
+import com.ruralnative.handsy.data.repository.AlphabetLessonRepository
+import com.ruralnative.handsy.data.repository.PhrasesLessonRepository
+import com.ruralnative.handsy.data.repository.UserRepository
+import com.ruralnative.handsy.di.AppModule
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import dagger.hilt.testing.TestInstallIn
+import javax.inject.Singleton
+
+@Module
+@TestInstallIn(components = [SingletonComponent::class], replaces = [AppModule::class])
+class FakeAppModule {
+
+    @Provides
+    @Singleton
+    fun provideLocalDatabase(
+        @ApplicationContext context: Context
+    ): AppDatabase {
+        return AppDatabase.getDatabase(context)
+    }
+
+    @Provides
+    @Singleton
+    fun provideAlphabetLessonDao(
+        database: AppDatabase
+    ): AlphabetLessonDao = database.alphabetLessonDao()
+
+    @Provides
+    @Singleton
+    fun providePhrasesLessonDao(
+        database: AppDatabase
+    ): PhrasesLessonDao = database.phrasesLessonDao()
+
+    @Provides
+    @Singleton
+    fun provideUserDao(
+        database: AppDatabase
+    ): UserDao = database.userDao()
+
+    @Provides
+    @Singleton
+    fun provideAlphabetLessonRepository(): AlphabetLessonRepository = AlphabetLessonRepository()
+
+    @Provides
+    @Singleton
+    fun providePhrasesLessonRepository(): PhrasesLessonRepository = PhrasesLessonRepository()
+
+    @Provides
+    @Singleton
+    fun provideUserRepository(): UserRepository = UserRepository()
+}
