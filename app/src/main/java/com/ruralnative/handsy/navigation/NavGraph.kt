2c4b9f7a0c753e8f211b925dce7cabaaea64eea8
John Berlin Leonor <johnberlinleonor@yahoo.com>
2024-01-19 15:01:27 +0800

    Update Navigation Graph
    Added codebase for Screen Entry composable function and modified the parameter used for the NavHost composable

--- app/src/main/java/com/ruralnative/handsy/navigation/NavGraph.kt
+++ app/src/main/java/com/ruralnative/handsy/navigation/NavGraph.kt
@@ -1,20 +1,27 @@
 package com.ruralnative.handsy.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.ruralnative.handsy.ui.entryUI.EntryScreen
+import com.ruralnative.handsy.ui.entryUI.EntryViewModel
 
 @Composable
 fun NavGraph(
-    navController: NavHostController
+    navHostController: NavHostController
 ) {
     NavHost(
-        navController = navController,
+        navController = navHostController,
         startDestination = "entry_screen"
     ) {
         composable(Screen.Entry.route) {
-            TODO("Add code to direct to EntryScreen")
+            val viewModel = hiltViewModel<EntryViewModel>()
+            EntryScreen(
+                modifier = Modifier,
+                navController = navHostController,
+                viewModel = viewModel)
         }
         composable(Screen.UserIntro.route) {
             TODO("Add code to direct to UserIntroScreen")
