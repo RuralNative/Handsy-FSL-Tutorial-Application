76c1ad949efd8d8fec44387faeacebaac9fd1061
John Berlin Leonor <johnberlinleonor@yahoo.com>
2024-01-19 14:10:38 +0800

    Added Routes for Available Composables
    Added composable functions for the NavHost composable for directing to different Composable screens

--- app/src/main/java/com/ruralnative/handsy/navigation/NavHost.kt
+++ app/src/main/java/com/ruralnative/handsy/navigation/NavHost.kt
@@ -1,20 +1,26 @@
 package com.ruralnative.handsy.navigation
 
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavController
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 
 @Composable
 fun NavGraph(
-    navController: NavController
+    navController: NavHostController
 ) {
     NavHost(
         navController = navController,
         startDestination = "entry_screen"
     ) {
-        composable("entry_screen") {
-
+        composable(Screen.Entry.route) {
+            TODO("Add code to direct to EntryScreen")
+        }
+        composable(Screen.UserIntro.route) {
+            TODO("Add code to direct to UserIntroScreen")
+        }
+        composable(Screen.Author.route) {
+            TODO("Add code to direct to AuthorMessageScreen")
         }
     }
 }
