9050ac2a3ae62742600a7c45b7e6ca7845a89df1
John Berlin Leonor <117552434+RuralNative@users.noreply.github.com>
2024-01-22 14:45:45 +0800

    Removed Author Message Screen
    Removed the Composable file for the Author Message Screen due to its unusability, and modified the Screen sealed class to adapt with the changes

--- app/src/main/java/com/ruralnative/handsy/navigation/Screen.kt
+++ app/src/main/java/com/ruralnative/handsy/navigation/Screen.kt
@@ -3,5 +3,4 @@ package com.ruralnative.handsy.navigation
 sealed class Screen(val route: String) {
     data object Entry: Screen(route = "entry_screen")
     data object UserIntro: Screen(route = "user_intro_screen")
-    data object Author: Screen(route = "author_message_screen")
 }
