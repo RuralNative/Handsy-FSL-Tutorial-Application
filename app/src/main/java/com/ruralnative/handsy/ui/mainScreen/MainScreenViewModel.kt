b81ab8e8298bf2556603beab2d84b633a1c03193
John Berlin Leonor <117552434+RuralNative@users.noreply.github.com>
2024-02-05 11:35:52 +0800

    Add Code to Update Constant State Update

--- app/src/main/java/com/ruralnative/handsy/ui/mainScreen/MainScreenViewModel.kt
+++ app/src/main/java/com/ruralnative/handsy/ui/mainScreen/MainScreenViewModel.kt
@@ -1,17 +1,32 @@
 package com.ruralnative.handsy.ui.mainScreen
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.ruralnative.handsy.data.repository.AlphabetLessonRepository
 import com.ruralnative.handsy.data.repository.PhrasesLessonRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 class MainScreenViewModel(
     private val alphabetRepository: AlphabetLessonRepository,
     private val phrasesRepository: PhrasesLessonRepository
-) {
-
+): ViewModel() {
     private val _uiState = MutableStateFlow(MainScreenState())
     val uiState: StateFlow<MainScreenState> = _uiState.asStateFlow()
 
+    init {
+        viewModelScope.launch {
+            alphabetRepository.allLessons.collect() {lessons ->
+                _uiState.value = _uiState.value.copy(alphabetLessons = lessons)
+            }
+        }
+        viewModelScope.launch {
+            phrasesRepository.allLessons.collect() {lessons ->
+                _uiState.value = _uiState.value.copy(phrasesLesson = lessons)
+            }
+        }
+    }
 }
