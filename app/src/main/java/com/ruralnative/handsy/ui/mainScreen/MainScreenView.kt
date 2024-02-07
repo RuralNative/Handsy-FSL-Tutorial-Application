41ab027fec5179e67d9f1cb53127fe36affc7de8
John Berlin Leonor <117552434+RuralNative@users.noreply.github.com>
2024-02-07 13:52:12 +0800

    Created View Class for MainScreen
    Created the View class for the MainScreen with initial yet empty composable codebase

--- app/src/main/java/com/ruralnative/handsy/ui/mainScreen/MainScreenView.kt
+++ app/src/main/java/com/ruralnative/handsy/ui/mainScreen/MainScreenView.kt
@@ -1,2 +1,35 @@
 package com.ruralnative.handsy.ui.mainScreen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.ruralnative.handsy.ui.theme.HandsyTheme
+
+@Preview
+@Composable
+fun MainScreen(
+) {
+    HandsyTheme {
+        Surface (
+            modifier = Modifier
+                .fillMaxSize()
+                .background(color = MaterialTheme.colorScheme.background)
+        ) {
+
+        }
+    }
+}
+
+@Composable
+fun LessonCard() {
+
+}
+
+@Composable
+fun LessonColumn() {
+
+}
