0dbc012db6c658f5e0996abc03da929ec8ebd40b
John Berlin Leonor <117552434+RuralNative@users.noreply.github.com>
2024-02-07 14:36:18 +0800

    Modified UI State of MainScreen
    Modified the properties of the MainScreenState to adapt with the ViewModel changes

--- app/src/main/java/com/ruralnative/handsy/ui/mainScreen/MainScreenState.kt
+++ app/src/main/java/com/ruralnative/handsy/ui/mainScreen/MainScreenState.kt
@@ -1,9 +1,6 @@
 package com.ruralnative.handsy.ui.mainScreen
 
-import com.ruralnative.handsy.data.entities.AlphabetLesson
-import com.ruralnative.handsy.data.entities.PhrasesLesson
-
 data class MainScreenState(
-    val alphabetLessons: List<LessonCard>? = null,
-    val phrasesLesson: List<LessonCard>? = null,
+    val alphabetLessons: List<LessonCardState>? = null,
+    val phrasesLesson: List<LessonCardState>? = null,
 )
