0540b5eac4b16f691f30010d96390e17ba8808c3
John Berlin Leonor <117552434+RuralNative@users.noreply.github.com>
2024-02-10 10:28:55 +0800

    Migrated Plugins Dependencies to Version Catalogs

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,6 @@ buildscript {
 plugins {
     id("com.android.application") version "8.2.1" apply false
     id("com.android.library") version "8.2.1" apply false
-
     id("org.jetbrains.kotlin.android") version "1.9.20" apply false
 
     id("com.google.devtools.ksp") version "1.9.20-1.0.13" apply false
