f60898dd7428404cd1761a4fa066d5fc5aa4d6d1
John Berlin Leonor <117552434+RuralNative@users.noreply.github.com>
2024-02-10 11:55:08 +0800

    Created Hilt Module for ViewModels
    Created a Hilt module for ViewModels use and scoping

---
+++ app/src/main/java/com/ruralnative/handsy/di/ViewModelModule.kt
@@ -0,0 +1,50 @@
+package com.ruralnative.handsy.di
+
+import com.ruralnative.handsy.data.dao.AlphabetLessonDao
+import com.ruralnative.handsy.data.dao.PhrasesLessonDao
+import com.ruralnative.handsy.data.dao.UserDao
+import com.ruralnative.handsy.data.repository.AlphabetLessonRepository
+import com.ruralnative.handsy.data.repository.PhrasesLessonRepository
+import com.ruralnative.handsy.data.repository.UserRepository
+import com.ruralnative.handsy.di.qualifiers.AlphabetDAO
+import com.ruralnative.handsy.di.qualifiers.AlphabetRepo
+import com.ruralnative.handsy.di.qualifiers.PhrasesDAO
+import com.ruralnative.handsy.di.qualifiers.PhrasesRepo
+import com.ruralnative.handsy.di.qualifiers.UserDAO
+import com.ruralnative.handsy.di.qualifiers.UserRepo
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.scopes.ViewModelScoped
+
+@Module
+@InstallIn(ViewModelModule::class)
+internal object ViewModelModule {
+
+    @Provides
+    @UserRepo
+    @ViewModelScoped
+    fun provideUserRepository(
+        @UserDAO dao: UserDao
+    ): UserRepository {
+        return UserRepository(dao)
+    }
+
+    @Provides
+    @AlphabetRepo
+    @ViewModelScoped
+    fun provideAlphabetRepository(
+        @AlphabetDAO dao: AlphabetLessonDao
+    ): AlphabetLessonRepository {
+        return AlphabetLessonRepository(dao)
+    }
+
+    @Provides
+    @PhrasesRepo
+    @ViewModelScoped
+    fun providePhrasesRepository(
+        @PhrasesDAO dao: PhrasesLessonDao
+    ): PhrasesLessonRepository {
+        return PhrasesLessonRepository(dao)
+    }
+}
