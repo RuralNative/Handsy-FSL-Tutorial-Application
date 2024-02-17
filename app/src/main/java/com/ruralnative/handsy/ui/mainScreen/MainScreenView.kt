0eb60341c35cf47c16488ec77aad5dd0b42667c9
John Berlin Leonor <117552434+RuralNative@users.noreply.github.com>
2024-02-17 14:55:18 +0800

    Modified MainScreen Codebase
    Added Context for use of the Composable functions and modified Card properties

--- app/src/main/java/com/ruralnative/handsy/ui/mainScreen/MainScreenView.kt
+++ app/src/main/java/com/ruralnative/handsy/ui/mainScreen/MainScreenView.kt
@@ -9,12 +9,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.tooling.preview.Preview
@@ -25,6 +27,7 @@ import com.ruralnative.handsy.ui.theme.HandsyTheme
 @Composable
 fun MainScreen(
 ) {
+    val context = LocalContext.current
     HandsyTheme {
         Surface (
             modifier = Modifier
@@ -36,9 +39,11 @@ fun MainScreen(
     }
 }
 
-@Preview
 @Composable
-fun LessonCard() {
+fun LessonCard(
+    modifier: Modifier,
+    lesson: LessonCardState
+) {
     Card(
         modifier = Modifier
             .fillMaxWidth()
@@ -47,7 +52,8 @@ fun LessonCard() {
                 enabled = true,
                 onClickLabel = "Lesson Card",
                 onClick = { /*TODO*/ }
-            )
+            ),
+        elevation = CardDefaults.elevatedCardElevation()
     ) {
        Column(
            modifier = Modifier
