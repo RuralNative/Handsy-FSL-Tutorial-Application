ea985080dd9cfabbe22674a2d4d511a6c7834db7
John Berlin Leonor <117552434+RuralNative@users.noreply.github.com>
2024-02-17 12:07:14 +0800

    Cleaned Up Module Imports
    Removed unused imports inside the DI Module

--- app/src/main/java/com/ruralnative/handsy/di/DatabaseModule.kt
+++ app/src/main/java/com/ruralnative/handsy/di/DatabaseModule.kt
@@ -1,26 +1,14 @@
 package com.ruralnative.handsy.di
 
 import android.content.Context
-import androidx.room.Room
 import com.ruralnative.handsy.data.AppDatabase
 import com.ruralnative.handsy.data.dao.AlphabetLessonDao
 import com.ruralnative.handsy.data.dao.PhrasesLessonDao
 import com.ruralnative.handsy.data.dao.UserDao
-import com.ruralnative.handsy.data.repository.AlphabetLessonRepository
-import com.ruralnative.handsy.data.repository.PhrasesLessonRepository
-import com.ruralnative.handsy.data.repository.UserRepository
-import com.ruralnative.handsy.di.qualifiers.AlphabetDAO
-import com.ruralnative.handsy.di.qualifiers.AlphabetRepo
-import com.ruralnative.handsy.di.qualifiers.Database
-import com.ruralnative.handsy.di.qualifiers.PhrasesDAO
-import com.ruralnative.handsy.di.qualifiers.PhrasesRepo
-import com.ruralnative.handsy.di.qualifiers.UserDAO
-import com.ruralnative.handsy.di.qualifiers.UserRepo
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
-import dagger.hilt.android.scopes.ViewModelScoped
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
