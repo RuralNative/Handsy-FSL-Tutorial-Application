e0bd31dfca92977c3099a202e854991684d90be6
John Berlin Leonor <117552434+RuralNative@users.noreply.github.com>
2024-02-18 08:38:53 +0800

    Added Properties for Lesson State
    Added the necessary properties for the State to be used by the Composable

--- app/src/main/java/com/ruralnative/handsy/ui/lessonScreen/LessonState.kt
+++ app/src/main/java/com/ruralnative/handsy/ui/lessonScreen/LessonState.kt
@@ -1,3 +1,7 @@
 package com.ruralnative.handsy.ui.lessonScreen
 
-data class LessonState()
+data class LessonState(
+    val lessonName: String,
+    val lessonDescription: String,
+    val lessonMediaResource: String
+)
