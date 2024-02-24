35999e562e1379dd105b0b856987973d6fbe247f
John Berlin Leonor <117552434+RuralNative@users.noreply.github.com>
2024-02-24 10:00:05 +0800

    Added Implementation for Window

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -105,6 +105,7 @@ dependencies {
     implementation(libs.navigation.fragment.ktx)
     implementation(libs.navigation.ui.ktx)
     implementation(libs.navigation.compose)
+    implementation(libs.window)
 
     // Unit Test Implementation Dependencies
     testImplementation(libs.core.common)
