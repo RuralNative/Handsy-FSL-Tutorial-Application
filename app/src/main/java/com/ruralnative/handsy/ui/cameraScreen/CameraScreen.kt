d62d7920ec2bc6d33caa08e80a934fc4fbca60b1
John Berlin Leonor <117552434+RuralNative@users.noreply.github.com>
2024-03-03 07:01:34 +0800

    Update Camera Screen
    Added code for showing either a Camera Preview if permission is granted, else a dialog if not

--- app/src/main/java/com/ruralnative/handsy/ui/cameraScreen/CameraScreen.kt
+++ app/src/main/java/com/ruralnative/handsy/ui/cameraScreen/CameraScreen.kt
@@ -8,39 +8,29 @@ import androidx.camera.view.PreviewView
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
 
 @Composable
-fun CameraScreen() {
-    val lensFacing = CameraSelector.LENS_FACING_FRONT
-    val lifecycleOwner = LocalLifecycleOwner.current
-    val context = LocalContext.current
-    val preview = Preview.Builder().build()
-    val previewView = remember {
-        PreviewView(context)
-    }
-    val cameraxSelector = CameraSelector.Builder().requireLensFacing(lensFacing).build()
-    LaunchedEffect(lensFacing) {
-        val cameraProvider = context.getCameraProvider()
-        cameraProvider.unbindAll()
-        cameraProvider.bindToLifecycle(lifecycleOwner, cameraxSelector, preview)
-        preview.setSurfaceProvider(previewView.surfaceProvider)
-    }
-    AndroidView(factory = { previewView }, modifier = Modifier.fillMaxSize())
-}
+fun CameraScreen(
+    viewModel: CameraViewModel = hiltViewModel()
+) {
 
-private suspend fun Context.getCameraProvider(): ProcessCameraProvider =
-    suspendCoroutine { continuation ->
-        ProcessCameraProvider.getInstance(this).also { cameraProvider ->
-            cameraProvider.addListener({
-                continuation.resume(cameraProvider.get())
-            }, ContextCompat.getMainExecutor(this))
-        }
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+    val cameraPermissionGranted = uiState.cameraPermissionGranted
+
+    if (cameraPermissionGranted) {
+        TODO("Show Camera Preview")
+    } else {
+        TODO("Request camera permission and show dialog where permission is not granted")
     }
+}
