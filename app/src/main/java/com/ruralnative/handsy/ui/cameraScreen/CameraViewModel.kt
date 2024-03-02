94a941229367296e3f3866196489adec55059c5a
John Berlin Leonor <117552434+RuralNative@users.noreply.github.com>
2024-03-03 06:53:13 +0800

    Create ViewModel for Camera
    Created a Hilt-enabled ViewModel for the Camera use, with initial code for checking camera use permission from the system

--- app/src/main/java/com/ruralnative/handsy/ui/cameraScreen/CameraViewModel.kt
+++ app/src/main/java/com/ruralnative/handsy/ui/cameraScreen/CameraViewModel.kt
@@ -1,4 +1,29 @@
 package com.ruralnative.handsy.ui.cameraScreen
 
-class CameraViewModel {
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import javax.inject.Inject
+
+@HiltViewModel
+class CameraViewModel @Inject constructor(
+    private val savedStateHandle: SavedStateHandle
+): ViewModel() {
+
+    private val _uiState = MutableStateFlow(CameraState())
+    val uiState: StateFlow<CameraState> = _uiState.asStateFlow()
+
+    fun requestPermission(context: Context) {
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            _uiState.value = _uiState.value.copy(cameraPermissionGranted = true)
+        }
+    }
 }
