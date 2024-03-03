fce5f0f793efe632671b066724f6402d8b88ceed
John Berlin Leonor <117552434+RuralNative@users.noreply.github.com>
2024-03-03 08:26:24 +0800

    Handled Permission Request for Camera
    Handled the permission request function for Camera component

--- app/src/main/java/com/ruralnative/handsy/ui/cameraScreen/CameraScreen.kt
+++ app/src/main/java/com/ruralnative/handsy/ui/cameraScreen/CameraScreen.kt
@@ -1,53 +1,45 @@
 package com.ruralnative.handsy.ui.cameraScreen
 
+import android.Manifest
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.PermissionRequired
+import com.google.accompanist.permissions.PermissionState
+import com.google.accompanist.permissions.rememberPermissionState
 
+@OptIn(ExperimentalPermissionsApi::class)
 @Composable
 fun CameraScreen(
     viewModel: CameraViewModel = hiltViewModel()
 ) {
-
-    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
-    val cameraPermissionGranted = uiState.cameraPermissionGranted
-
-    viewModel.checkAndUpdatePermission(LocalContext.current)
-
-    if (cameraPermissionGranted) {
-        TODO("Show Camera Preview")
-    } else {
-        TODO("Request camera permission and show dialog where permission is not granted")
+    val permissionState = rememberPermissionState(permission = Manifest.permission.CAMERA)
+    CameraPermission(permissionState)
+    SideEffect {
+        permissionState.launchPermissionRequest()
     }
 }
 
 @OptIn(ExperimentalPermissionsApi::class)
 @Composable
-fun CameraPermissionRequest() {
-    // Create a permission state for the camera permission
-    val cameraPermissionState = rememberPermissionState(Manifest.permission.CAMERA)
-
-    when (cameraPermissionState.status) {
-        // If the permission is granted, you can use the camera feature
-        PermissionStatus.Granted -> {
-            Text("Camera permission Granted")
-            // Add your camera feature UI here
-        }
-        is PermissionStatus.Denied -> {
-            // If the permission is denied, show a message and a button to request the permission
-            Column {
-                Text(if (cameraPermissionState.status.shouldShowRationale) {
-                    "The camera is important for this app. Please grant the permission."
-                } else {
-                    "Camera permission required for this feature to be available. Please grant the permission."
-                })
-                Button(onClick = { cameraPermissionState.launchPermissionRequest() }) {
-                    Text("Request permission")
-                }
-            }
-        }
+private fun CameraPermission(
+    permissionState: PermissionState,
+) {
+    PermissionRequired(
+        permissionState = permissionState,
+        permissionNotGrantedContent = { /* ... */ },
+        permissionNotAvailableContent = { /* ... */ }
+    ) {
+        Camera()
     }
 }
 
+@Composable
+private fun Camera() {
+    TODO("Initialize CameraX")
+}
