0707168e3ac9422f4e2eca0836be53851b041c13
John Berlin Leonor <117552434+RuralNative@users.noreply.github.com>
2024-03-03 08:44:39 +0800

    Update Camera UI
    Added the necessary code to initialize a CameraX component with Preview use case upon successful permission request. Additionally, modified the manifest file to declare that camera hardware is a required condition for Camera use

--- app/src/main/java/com/ruralnative/handsy/ui/cameraScreen/CameraScreen.kt
+++ app/src/main/java/com/ruralnative/handsy/ui/cameraScreen/CameraScreen.kt
@@ -1,11 +1,21 @@
 package com.ruralnative.handsy.ui.cameraScreen
 
 import android.Manifest
+import androidx.camera.core.CameraSelector
+import androidx.camera.view.LifecycleCameraController
+import androidx.camera.view.PreviewView
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
@@ -35,11 +45,21 @@ private fun CameraPermission(
         permissionNotGrantedContent = { /* ... */ },
         permissionNotAvailableContent = { /* ... */ }
     ) {
-        Camera()
+        CameraComponent()
     }
 }
 
 @Composable
-private fun Camera() {
-    TODO("Initialize CameraX")
+private fun CameraComponent() {
+    val context = LocalContext.current
+    val previewView: PreviewView = remember { PreviewView(context) }
+    val cameraController = remember { LifecycleCameraController(context) }
+    val lifecycleOwner = LocalLifecycleOwner.current
+    cameraController.bindToLifecycle(lifecycleOwner)
+    cameraController.cameraSelector = CameraSelector.DEFAULT_FRONT_CAMERA
+    previewView.controller = cameraController
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        AndroidView(factory = { previewView }, modifier = Modifier.fillMaxSize())
+    }
 }
