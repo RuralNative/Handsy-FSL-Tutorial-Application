3253a46097dd49ed2e32766fd15e6d1551f7950d
John Berlin Leonor <117552434+RuralNative@users.noreply.github.com>
2024-03-07 16:50:01 +0800

    Update CameraState
    Added necessary properties for use of the Camera component, wherein codebase is based from the content of the ViewModel found in sample file from MediaPipe

--- app/src/main/java/com/ruralnative/handsy/ui/cameraScreen/CameraState.kt
+++ app/src/main/java/com/ruralnative/handsy/ui/cameraScreen/CameraState.kt
@@ -1,5 +1,8 @@
 package com.ruralnative.handsy.ui.cameraScreen
 
 data class CameraState (
-    val value: Int = 0
+    val delegate: Int = GestureRecognizerHelper.DELEGATE_CPU,
+    val minimumHandDetectionConfidence: Float = GestureRecognizerHelper.DEFAULT_HAND_DETECTION_CONFIDENCE,
+    val minimumHandTrackingConfidence: Float = GestureRecognizerHelper.DEFAULT_HAND_TRACKING_CONFIDENCE,
+    val minimumHandPresenceConfidence: Float = GestureRecognizerHelper.DEFAULT_HAND_PRESENCE_CONFIDENCE
 )
