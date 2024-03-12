6b6e306cedbbe1d81d56b3916feede2d04f15322
John Berlin Leonor <117552434+RuralNative@users.noreply.github.com>
2024-03-12 14:07:32 +0800

    Added Splash Icon Logo
    Added a modified SVG file matching size requirements of the Splash Screen API for use

--- app/src/main/java/com/ruralnative/handsy/navigation/Screen.kt
+++ app/src/main/java/com/ruralnative/handsy/navigation/Screen.kt
@@ -1,5 +1,8 @@
 package com.ruralnative.handsy.navigation
 
+/**
+ * Type-safe object containing String-typed routes which serves as unique identifiers for each Composable screens in the Navigation Graph
+ */
 sealed class Screen(val route: String) {
     data object Entry: Screen(route = "entry_screen")
     data object UserIntro: Screen(route = "user_intro_screen")
