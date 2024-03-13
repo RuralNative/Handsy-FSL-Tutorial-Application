866a1edffdf56e8bf088d976d3e544e4af190590
John Berlin Leonor <117552434+RuralNative@users.noreply.github.com>
2024-03-13 14:23:58 +0800

    Modified BottomBar Codebase
    Added formatting and design changes adherent to Material 3 principles and complete the codebase for functionality

--- app/src/main/java/com/ruralnative/handsy/compose/components/BottomBar.kt
+++ app/src/main/java/com/ruralnative/handsy/compose/components/BottomBar.kt
@@ -1,63 +1,66 @@
 package com.ruralnative.handsy.compose.components
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.BottomAppBar
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.NavigationBarItemColors
 import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collection.mutableVectorOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.ruralnative.handsy.R
-import com.ruralnative.handsy.compose.cameraNavigation
-import com.ruralnative.handsy.compose.mainNavigation
-import com.ruralnative.handsy.compose.statsNavigation
 
 @Preview
 @Composable
 fun BottomBar(
 ) {
-    var selectedItem by remember {mutableIntStateOf(0)}
+    var selectedItem by remember {mutableIntStateOf(2)}
 
-    NavigationBar {
+    NavigationBar(
+        modifier = Modifier
+            .height(80.dp),
+        containerColor = MaterialTheme.colorScheme.surface,
+        contentColor = Color.White,
+        tonalElevation = 3.dp,
+        windowInsets = WindowInsets.navigationBars,
+    ) {
         NavigationBarItem(
-            selected = ,
-            onClick = { /*TODO*/ },
-            icon = { /*TODO*/ },
+            selected = selectedItem == 1,
+            onClick = { selectedItem = 1 },
+            icon = { LessonIcon(modifier = Modifier) },
             enabled = true,
             label = { Text(text = "Lessons") },
             alwaysShowLabel = false,
             colors = NavigationBarItemDefaults.colors(),
         )
         NavigationBarItem(
-            selected = ,
-            onClick = { /*TODO*/ },
-            icon = { /*TODO*/ }
+            selected = selectedItem == 2,
+            onClick = { selectedItem = 2 },
+            icon = { HomeIcon(modifier = Modifier) },
+            enabled = true,
+            label = { Text(text = "Home") },
+            alwaysShowLabel = false,
+            colors = NavigationBarItemDefaults.colors(),
         )
         NavigationBarItem(
-            selected = ,
-            onClick = { /*TODO*/ },
-            icon = { /*TODO*/ }
+            selected = selectedItem == 3,
+            onClick = { selectedItem = 3 },
+            icon = { CameraIcon(modifier = Modifier) },
+            enabled = true,
+            label = { Text(text = "Camera") },
+            alwaysShowLabel = false,
+            colors = NavigationBarItemDefaults.colors(),
         )
     }
 }
