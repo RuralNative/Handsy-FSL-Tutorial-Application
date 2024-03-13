74eb1c19f657c814e36f5b2595305c9f553703ed
John Berlin Leonor <117552434+RuralNative@users.noreply.github.com>
2024-03-13 13:53:20 +0800

    Create Icons for Bottom Bar
    Created separate Icons for the use of the BottomBar Navigation Bar

--- app/src/main/java/com/ruralnative/handsy/compose/components/BottomBar.kt
+++ app/src/main/java/com/ruralnative/handsy/compose/components/BottomBar.kt
@@ -7,12 +7,24 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemColors
+import androidx.compose.material3.NavigationBarItemDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collection.mutableVectorOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -25,41 +37,63 @@ import com.ruralnative.handsy.compose.statsNavigation
 @Composable
 fun BottomBar(
 ) {
-    BottomAppBar(
-        containerColor = MaterialTheme.colorScheme.primary,
-        contentColor = Color.White,
-        contentPadding = PaddingValues(2.dp)
-    ) {
-        Row (
-            modifier = Modifier
-                .fillMaxSize(1F),
-            horizontalArrangement = Arrangement.SpaceEvenly,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            IconButton(onClick = mainNavigation ) {
-                Image(
-                    modifier = Modifier
-                        .size(100.dp),
-                    painter = painterResource(id = R.drawable.icon_lessons),
-                    contentDescription = "Lessons Icon"
-                )
-            }
-            IconButton(onClick = cameraNavigation) {
-                Image(
-                    modifier = Modifier
-                        .size(100.dp),
-                    painter = painterResource(id = R.drawable.icon_camera),
-                    contentDescription = "Camera Icon"
-                )
-            }
-            IconButton(onClick = statsNavigation) {
-                Image(
-                    modifier = Modifier
-                        .size(100.dp),
-                    painter = painterResource(id = R.drawable.icon_stats),
-                    contentDescription = "User Stats Icon"
-                )
-            }
-        }
+    var selectedItem by remember {mutableIntStateOf(0)}
+
+    NavigationBar {
+        NavigationBarItem(
+            selected = ,
+            onClick = { /*TODO*/ },
+            icon = { /*TODO*/ },
+            enabled = true,
+            label = { Text(text = "Lessons") },
+            alwaysShowLabel = false,
+            colors = NavigationBarItemDefaults.colors(),
+        )
+        NavigationBarItem(
+            selected = ,
+            onClick = { /*TODO*/ },
+            icon = { /*TODO*/ }
+        )
+        NavigationBarItem(
+            selected = ,
+            onClick = { /*TODO*/ },
+            icon = { /*TODO*/ }
+        )
     }
+}
+
+@Composable
+private fun HomeIcon(
+    modifier: Modifier
+) {
+    Icon(
+        painter = painterResource(id = R.drawable.icon_home),
+        contentDescription = "Navigation Button for Home Screen",
+        modifier = modifier,
+        tint = Color.Unspecified
+    )
+}
+
+@Composable
+private fun LessonIcon(
+    modifier: Modifier
+) {
+    Icon(
+        painter = painterResource(id = R.drawable.icon_lesson),
+        contentDescription = "Navigation Button for Lessons Screen",
+        modifier = modifier,
+        tint = Color.Unspecified
+    )
+}
+
+@Composable
+private fun CameraIcon(
+    modifier: Modifier
+) {
+    Icon(
+        painter = painterResource(id = R.drawable.icon_camera),
+        contentDescription = "Navigation Button for Camera Screen",
+        modifier = modifier,
+        tint = Color.Unspecified
+    )
 }
