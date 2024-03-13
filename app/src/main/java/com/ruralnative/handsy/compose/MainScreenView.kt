52a04de1daf45c9caebf730baec2b5119232fb7a
John Berlin Leonor <117552434+RuralNative@users.noreply.github.com>
2024-03-13 15:05:26 +0800

    Implement Full Screen for Main Screen
    Implement window insets and other modifiers to ensure the MainScreen fit the entire screen

--- app/src/main/java/com/ruralnative/handsy/compose/MainScreenView.kt
+++ app/src/main/java/com/ruralnative/handsy/compose/MainScreenView.kt
@@ -10,9 +10,12 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContent
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.windowInsetsBottomHeight
+import androidx.compose.foundation.layout.windowInsetsTopHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
@@ -63,6 +66,10 @@ fun MainScreen(
 
     HandsyTheme {
         Scaffold (
+            modifier = Modifier
+                .fillMaxSize()
+                .windowInsetsTopHeight(WindowInsets.navigationBars)
+                .windowInsetsBottomHeight(WindowInsets.navigationBars),
             topBar = { TopBar() },
             bottomBar = { BottomBar() },
             containerColor = MaterialTheme.colorScheme.background,
