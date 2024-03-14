218ee535ce87ac0e705dfd4bca10947ad78c9c58
John Berlin Leonor <117552434+RuralNative@users.noreply.github.com>
2024-03-14 11:04:30 +0800

    Modified Data and List
    Modified the Navigation Data class which info is derived from and the List to contain routes for Navigation purposes

--- app/src/main/java/com/ruralnative/handsy/compose/components/BottomBar.kt
+++ app/src/main/java/com/ruralnative/handsy/compose/components/BottomBar.kt
@@ -27,6 +27,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -36,38 +37,59 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
 import com.ruralnative.handsy.R
 import com.ruralnative.handsy.navigation.BottomNavigation
+import com.ruralnative.handsy.navigation.Screen
 import com.ruralnative.handsy.ui.NunitoFontFamily
 
 val navigationItems = listOf(
     BottomNavigation(
-        title = "Lessons",
+        title = "Main",
+        route = Screen.MainScreen.route,
         selectedIcon = Icons.Filled.Book,
         unselectedIcon = Icons.Outlined.Book
     ),
     BottomNavigation(
         title = "Home",
+        route = Screen.MainScreen.route,
         selectedIcon = Icons.Filled.Home,
         unselectedIcon = Icons.Outlined.Home
     ),
     BottomNavigation(
         title = "Camera",
+        route = Screen.CameraScreen.route,
         selectedIcon = Icons.Filled.Camera,
         unselectedIcon = Icons.Outlined.Camera
     )
 )
 
-@OptIn(ExperimentalLayoutApi::class)
-@Preview
 @Composable
 fun BottomBar(
+    navController: NavHostController
 ) {
-    var selectedItem by remember {mutableIntStateOf(2)}
+    var selectedItemIndex by rememberSaveable {
+        mutableIntStateOf(1)
+    }
 
     NavigationBar(
     ) {
-        
+        navigationItems.forEachIndexed { index, item ->
+            NavigationBarItem(
+                selected = selectedItemIndex == index,
+                onClick = {
+                    selectedItemIndex = index
+                    navController.navigate(item.route)
+                },
+                icon = {},
+                modifier = ,
+                enabled = ,
+                label = {},
+                alwaysShowLabel = ,
+                colors = ,
+            )
+        }
     }
 }
 
--- app/src/main/java/com/ruralnative/handsy/navigation/BottomNavigation.kt
+++ app/src/main/java/com/ruralnative/handsy/navigation/BottomNavigation.kt
@@ -11,6 +11,7 @@ import androidx.compose.ui.graphics.vector.ImageVector
 
 data class BottomNavigation(
     val title: String,
+    val route: String,
     val selectedIcon: ImageVector,
     val unselectedIcon: ImageVector
 )
