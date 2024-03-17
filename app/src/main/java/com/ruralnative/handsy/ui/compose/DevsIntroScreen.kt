df5c70d074ab722526c514d19029d4167562e98d
John Berlin Leonor <johnberlinleonor@yahoo.com>
2024-03-17 11:59:26 +0800

    Added Design Format for DevsIntro

--- app/src/main/java/com/ruralnative/handsy/ui/compose/DevsIntroScreen.kt
+++ app/src/main/java/com/ruralnative/handsy/ui/compose/DevsIntroScreen.kt
@@ -1,10 +1,15 @@
 package com.ruralnative.handsy.ui.compose
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -17,6 +22,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.ruralnative.handsy.R
 import com.ruralnative.handsy.ui.NunitoFontFamily
@@ -27,23 +33,48 @@ import com.ruralnative.handsy.ui.viewmodel.UserIntroViewModel
 fun DevsIntroScreen(
 ) {
     //onNavigateToMainScreen: () -> Unit
-    TODO("Integrate all Composables")
-    HeaderText()
-    DevsIntroImage()
+
+    ConstraintLayout(
+        modifier = Modifier
+            .fillMaxHeight()
+            .fillMaxWidth()
+            .safeContentPadding()
+            .background(MaterialTheme.colorScheme.background)
+    ) {
+        val (headerContainer, imageContainer) = createRefs()
+        HeaderText(
+            modifier = Modifier
+                .constrainAs(headerContainer) {
+                    start.linkTo(parent.start, margin = 16.dp)
+                    top.linkTo(parent.top, margin = 16.dp)
+                    end.linkTo(parent.end, margin = 16.dp)
+                    bottom.linkTo(imageContainer.top)
+                }
+                .padding(16.dp)
+        )
+        DevsIntroImage(
+            modifier = Modifier
+                .constrainAs(imageContainer) {
+                    start.linkTo(parent.start)
+                    top.linkTo(parent.top)
+                    end.linkTo(parent.end)
+                    bottom.linkTo(parent.bottom)
+                }
+        )
+    }
 }
 
-@Preview
 @Composable
-private fun HeaderText(
-) {
+private fun HeaderText(modifier: Modifier) {
     Column(
+        modifier = modifier,
         horizontalAlignment = Alignment.Start,
         verticalArrangement = Arrangement.Top
     ) {
         Text(
             text = stringResource(R.string.author_message_one),
             color = MaterialTheme.colorScheme.primary,
-            fontSize = 36.sp,
+            fontSize = 32.sp,
             fontWeight = FontWeight.ExtraBold,
             fontFamily = NunitoFontFamily
         )
@@ -52,22 +83,23 @@ private fun HeaderText(
                 .padding(top = 8.dp),
             text = stringResource(R.string.author_message_two),
             color = MaterialTheme.colorScheme.secondary,
-            fontSize = 22.sp,
+            fontSize = 16.sp,
             fontWeight = FontWeight.Normal,
             fontFamily = NunitoFontFamily
         )
     }
 }
 
-@Preview
 @Composable
-private fun DevsIntroImage() {
-    Box() {
+private fun DevsIntroImage(modifier: Modifier) {
+    Box(
+        modifier = modifier
+    ) {
         Image(
             painter = painterResource(id = R.drawable.author_message_media),
             contentDescription = null,
             modifier = Modifier
-                .size(270.dp)
+                .size(360.dp)
         )
     }
 }
