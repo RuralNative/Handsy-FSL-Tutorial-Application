dba27cde742f7932675ad9537d286189f139a95a
John Berlin Leonor <johnberlinleonor@yahoo.com>
2024-03-30 07:39:45 +0800

    Implement Listener to ViewModel
    Implemented Listener with its member methods within the ViewModel, wherein each methods will be automatically called by MediaPipe for every recognized results from LiveStream

--- app/src/main/java/com/ruralnative/handsy/ui/viewmodel/CameraLiveStreamViewModel.kt
+++ app/src/main/java/com/ruralnative/handsy/ui/viewmodel/CameraLiveStreamViewModel.kt
@@ -2,17 +2,37 @@ package com.ruralnative.handsy.ui.viewmodel
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
-import com.ruralnative.handsy.ui.state.CameraState
+import androidx.lifecycle.viewModelScope
+import com.ruralnative.handsy.ui.state.CameraLiveStreamState
+import com.ruralnative.handsy.util.GestureRecognizerListener
+import com.ruralnative.handsy.util.ResultBundle
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class CameraLiveStreamViewModel @Inject constructor(
     private val savedStateHandle: SavedStateHandle
-): ViewModel() {
-    private val _uiState = MutableStateFlow(CameraState())
-    val uiState: StateFlow<CameraState> = _uiState.asStateFlow()
+): ViewModel(), GestureRecognizerListener {
+
+    private val _uiState = MutableStateFlow(CameraLiveStreamState(results = emptyList()))
+    val uiState: StateFlow<CameraLiveStreamState> = _uiState.asStateFlow()
+
+    override fun onError(error: String, errorCode: Int) {
+        TODO("Update UI to Show Error")
+    }
+
+    override fun onResults(resultBundle: ResultBundle) {
+        viewModelScope.launch {
+            _uiState.value = _uiState.value.copy(
+                results = resultBundle.results,
+                inferenceTime = resultBundle.inferenceTime,
+                inputImageHeight = resultBundle.inputImageHeight,
+                inputImageWidth = resultBundle.inputImageWidth
+            )
+        }
+    }
 }
