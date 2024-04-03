64e3a219187ba9265ecb7d5dd20a1a6eebf3c1a0
John Berlin Leonor <johnberlinleonor@yahoo.com>
2024-04-03 11:25:42 +0800

    Update CameraPermissionRequest.kt
    Added TODO for main function and added initial outline of PermissionRequestDialog

--- app/src/main/java/com/ruralnative/handsy/ui/compose/components/CameraPermissionRequest.kt
+++ app/src/main/java/com/ruralnative/handsy/ui/compose/components/CameraPermissionRequest.kt
@@ -1,5 +1,6 @@
 package com.ruralnative.handsy.ui.compose.components
 
+import androidx.compose.material3.AlertDialog
 import androidx.compose.runtime.Composable
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.PermissionState
@@ -21,23 +22,20 @@ private fun CameraPermissionRequest(
     if (cameraPermissionState.status.isGranted) {
         cameraScreen()
     } else {
-        Column {
-            val textToShow = if (cameraPermissionState.status.shouldShowRationale) {
-                // If the user has denied the permission but the rationale can be shown,
-                // then gently explain why the app requires this permission
-                "The camera is important for this app. Please grant the permission."
-            } else {
-                // If it's the first time the user lands on this feature, or the user
-                // doesn't want to be asked again for this permission, explain that the
-                // permission is required
-                "Camera permission required for this feature to be available. " +
-                        "Please grant the permission"
-            }
-            Text(textToShow)
-            Button(onClick = { cameraPermissionState.launchPermissionRequest() }) {
-                Text("Request permission")
-            }
-        }
-        TODO("Implement Dialog for Camera Permission Request")
+        TODO("Implement UI For Rationale")
+        TODO("Request Camera Permission with different Use Cases")
+        TODO("If Request returns Yes, show cameraScreen()")
+        TODO("If Request returns No, show CameraPermissionDenied()")
     }
+}
+
+@Composable
+private fun PermissionRequestDialog() {
+    AlertDialog(
+        title = null,
+        text = null,
+        icon = null,
+        onDismissRequest = { /*TODO*/ },
+        confirmButton = { /*TODO*/ }
+    )
 }
