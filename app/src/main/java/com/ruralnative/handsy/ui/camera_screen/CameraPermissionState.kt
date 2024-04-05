15308067d127c91781767d220fac21ae281062b5
John Berlin Leonor <johnberlinleonor@yahoo.com>
2024-04-05 11:25:33 +0800

    Update CameraPermissionState.kt
    Added KDocs for better development effort

--- app/src/main/java/com/ruralnative/handsy/ui/camera_screen/CameraPermissionState.kt
+++ app/src/main/java/com/ruralnative/handsy/ui/camera_screen/CameraPermissionState.kt
@@ -1,11 +1,20 @@
 package com.ruralnative.handsy.ui.camera_screen
 
+/**
+ * Represents the state of the camera permission.
+ *
+ * @property cameraPermissionKey An integer key representing the camera permission status.
+ * - `0` (default key): Permission is denied and requires no rationale.
+ * - `1`: Permission is denied and requires rationale.
+ * - `2`: Permission is granted.
+ */
 data class CameraPermissionState(
+    
     /**
-     * Int Key representing Camera Permission
-     * If 0 (default key), Permission is denied and requires no rationale
-     * If 1, Permission is denied and requires rationale
-     * If 2, Permission is granted
+     * An integer key representing the camera permission status.
+     * - `0` (default key): Permission is denied and requires no rationale.
+     * - `1`: Permission is denied and requires rationale.
+     * - `2`: Permission is granted.
      */
     val cameraPermissionKey: Int = 0
 )
