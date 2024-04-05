6ddce941a6b25e38a08a37b4e517cecea8e1b2f8
John Berlin Leonor <johnberlinleonor@yahoo.com>
2024-04-05 11:46:20 +0800

    Update CameraPermissionViewModel.kt
    Added method to check value of the State key and navigates promptly to what screen

--- app/src/main/java/com/ruralnative/handsy/ui/camera_screen/CameraPermissionViewModel.kt
+++ app/src/main/java/com/ruralnative/handsy/ui/camera_screen/CameraPermissionViewModel.kt
@@ -1,9 +1,12 @@
 package com.ruralnative.handsy.ui.camera_screen
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -22,5 +25,32 @@ class CameraPermissionViewModel @Inject constructor() : ViewModel() {
      */
     fun setCameraPermissionKey(key: Int) {
         _uiState.value = CameraPermissionState(key)
+    }
+
+    /**
+     * Initializes the camera screen based on the current camera permission state.
+     *
+     * This function listens to changes in the camera permission state and navigates to the appropriate screen
+     * based on the permission key. It uses a coroutine launched in the ViewModel's scope to collect the latest
+     * state from the [uiState] flow and perform navigation accordingly.
+     *
+     * @param navigateToCameraScreen A lambda function to navigate to the camera screen.
+     * @param navigateToRationaleScreen A lambda function to navigate to the rationale screen.
+     * @param navigateToDefaultScreen A lambda function to navigate to the default screen.
+     */
+    fun initializeCameraScreen(
+        navigateToCameraScreen: () -> Unit,
+        navigateToRationaleScreen: () -> Unit,
+        navigateToDefaultScreen: () -> Unit
+    ) {
+        viewModelScope.launch {
+            uiState.collectLatest { state ->
+                when (state.cameraPermissionKey) {
+                    0 -> navigateToDefaultScreen()
+                    1 -> navigateToRationaleScreen()
+                    2 -> navigateToCameraScreen()
+                }
+            }
+        }
     }
 }
