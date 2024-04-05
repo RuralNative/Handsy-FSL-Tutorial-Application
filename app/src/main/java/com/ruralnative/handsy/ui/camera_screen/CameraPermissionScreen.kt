586f1955d7f93f6557a7e036ec79e44d4472feac
John Berlin Leonor <johnberlinleonor@yahoo.com>
2024-04-05 11:50:00 +0800

    Update CameraPermissionScreen.kt
    Implemented VM method initializeCameraScreen() with passed parameters

--- app/src/main/java/com/ruralnative/handsy/ui/camera_screen/CameraPermissionScreen.kt
+++ app/src/main/java/com/ruralnative/handsy/ui/camera_screen/CameraPermissionScreen.kt
@@ -3,6 +3,7 @@ package com.ruralnative.handsy.ui.camera_screen
 import androidx.compose.material3.AlertDialog
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
@@ -11,12 +12,15 @@ import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.PermissionStatus
 import com.google.accompanist.permissions.rememberPermissionState
 import com.google.accompanist.permissions.shouldShowRationale
+import kotlinx.coroutines.delay
 
 @Composable
 @OptIn(ExperimentalPermissionsApi::class)
 fun CameraPermissionRequest(
     viewModel: CameraPermissionViewModel,
-    cameraScreen: @Composable () -> Unit
+    onNavigateToDefaultScreen: () -> Unit,
+    onNavigateToRationaleScreen: () -> Unit,
+    onNavigateToCameraScreen: () -> Unit
 ) {
 
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
@@ -28,28 +32,20 @@ fun CameraPermissionRequest(
     LaunchedEffect(cameraPermissionState) {
         when (cameraPermissionState.status) {
             is PermissionStatus.Granted -> {
-                LaunchedEffect(Unit) {
-                    cameraScreen()
-                }
+                viewModel.setCameraPermissionKey(2)
             }
             is PermissionStatus.Denied -> {
                 if (cameraPermissionState.status.shouldShowRationale) {
-                    TODO("Show Rationale through Composable ()")
+                    viewModel.setCameraPermissionKey(1)
                 } else {
-                    TODO("Show Scaffold UI with Direction to Settings")
+                    viewModel.setCameraPermissionKey(0)
                 }
             }
         }
+        viewModel.initializeCameraScreen(
+            onNavigateToDefaultScreen,
+            onNavigateToRationaleScreen,
+            onNavigateToCameraScreen
+        )
     }
-}
-
-@Composable
-private fun PermissionRequestDialog() {
-    AlertDialog(
-        title = null,
-        text = null,
-        icon = null,
-        onDismissRequest = { /*TODO*/ },
-        confirmButton = { /*TODO*/ }
-    )
 }
