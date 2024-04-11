099a831ec86e3caf68acf229387af6e6aa7310df
John Berlin Leonor <johnberlinleonor@yahoo.com>
2024-04-11 16:35:23 +0800

    Relocated Backhandler

--- app/src/main/java/com/ruralnative/handsy/navigation/BackHandlers.kt
+++ app/src/main/java/com/ruralnative/handsy/navigation/BackHandlers.kt
@@ -1,12 +1,10 @@
-package com.ruralnative.handsy.ui.components
+package com.ruralnative.handsy.navigation
 
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.BackHandler
-import androidx.compose.foundation.Image
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
