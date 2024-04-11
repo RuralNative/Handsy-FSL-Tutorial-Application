d982cad25912d6252196e7720ed36f962eaaa80a
John Berlin Leonor <johnberlinleonor@yahoo.com>
2024-04-11 14:26:12 +0800

    Update BackHandlers.kt
    Added Dialog with initial codebase for Default Back Handler and removed LessonBackHandler()

--- app/src/main/java/com/ruralnative/handsy/ui/components/BackHandlers.kt
+++ app/src/main/java/com/ruralnative/handsy/ui/components/BackHandlers.kt
@@ -2,6 +2,11 @@ package com.ruralnative.handsy.ui.components
 
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.BackHandler
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
@@ -11,28 +16,82 @@ public fun DefaultBackHandler(
 ) {
     val application = LocalContext.current as ComponentActivity
     BackHandler(backHandlerState) {
+        AlertDialog(
+            icon = {
+                Icon(icon, contentDescription = "Example Icon")
+            },
+            title = {
+                Text(text = dialogTitle)
+            },
+            text = {
+                Text(text = dialogText)
+            },
+            onDismissRequest = {
+                onDismissRequest()
+            },
+            confirmButton = {
+                TextButton(
+                    onClick = {
+                        onConfirmation()
+                    }
+                ) {
+                    Text("Confirm")
+                }
+            },
+            dismissButton = {
+                TextButton(
+                    onClick = {
+                        onDismissRequest()
+                    }
+                ) {
+                    Text("Dismiss")
+                }
+            }
+        )
         application.finish()
         TODO("Prompt a Dialog with Option to Exit App")
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-public fun DisabledBackHandler(
-    backHandlerState: Boolean
+private fun DefaultDialog(
 ) {
-    BackHandler(backHandlerState) {
-        true
-    }
-}
-
-@Composable
-public fun ScaffoldBackHandler(
-    backHandlerState: Boolean,
-    onBackNavigation: () -> Unit
-) {
-    BackHandler(backHandlerState) {
-        TODO("Prompt a Dialog with Option to LessonList Screen")
-    }
+    AlertDialog(
+        icon = {
+            Icon(
+                icon,
+                contentDescription = "Example Icon"
+            )
+        },
+        title = {
+            Text(text = dialogTitle)
+        },
+        text = {
+            Text(text = dialogText)
+        },
+        onDismissRequest = {
+            TODO("On Dismiss Request")
+        },
+        confirmButton = {
+            TextButton(
+                onClick = {
+                    TODO("Confirm Button")
+                }
+            ) {
+                Text("Confirm")
+            }
+        },
+        dismissButton = {
+            TextButton(
+                onClick = {
+                    TODO("Dismiss Button")
+                }
+            ) {
+                Text("Dismiss")
+            }
+        }
+    )
 }
 
 @Composable
@@ -42,5 +101,14 @@ public fun LessonBackHandler(
 ) {
     BackHandler(backHandlerState) {
         TODO("Prompt a Dialog with Option to LessonList Screen")
+    }
+}
+
+@Composable
+public fun DisabledBackHandler(
+    backHandlerState: Boolean
+) {
+    BackHandler(backHandlerState) {
+        true
     }
 }
