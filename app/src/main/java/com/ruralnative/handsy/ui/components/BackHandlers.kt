5d76f3d0c96427b70df69194c59c3da19fe536f8
John Berlin Leonor <johnberlinleonor@yahoo.com>
2024-04-11 16:13:37 +0800

    Added Initial Codebase for DefaultDialog
    Modified arguments passed for parameters of DefaultDialog and added values within the strings XML for use of the dialog

--- app/src/main/java/com/ruralnative/handsy/ui/components/BackHandlers.kt
+++ app/src/main/java/com/ruralnative/handsy/ui/components/BackHandlers.kt
@@ -2,73 +2,68 @@ package com.ruralnative.handsy.ui.components
 
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.Image
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import com.ruralnative.handsy.R
 
 @Composable
 public fun DefaultBackHandler(
     backHandlerState: Boolean
 ) {
     val application = LocalContext.current as ComponentActivity
+    var openDialog by remember {
+        mutableStateOf(false)
+    }
+
     BackHandler(backHandlerState) {
-        AlertDialog(
-            icon = {
-                Icon(icon, contentDescription = "Example Icon")
-            },
-            title = {
-                Text(text = dialogTitle)
-            },
-            text = {
-                Text(text = dialogText)
-            },
-            onDismissRequest = {
-                onDismissRequest()
-            },
-            confirmButton = {
-                TextButton(
-                    onClick = {
-                        onConfirmation()
-                    }
-                ) {
-                    Text("Confirm")
-                }
-            },
-            dismissButton = {
-                TextButton(
-                    onClick = {
-                        onDismissRequest()
-                    }
-                ) {
-                    Text("Dismiss")
-                }
-            }
-        )
-        application.finish()
-        TODO("Prompt a Dialog with Option to Exit App")
+        openDialog = true
+    }
+
+    if (openDialog) {
+        DefaultDialog(
+            application = application,
+            dialogState = openDialog)
     }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun DefaultDialog(
+    application: ComponentActivity,
+    dialogState: Boolean
 ) {
     AlertDialog(
         icon = {
             Icon(
-                icon,
-                contentDescription = "Example Icon"
+                painter = painterResource(R.drawable.mascot),
+                contentDescription = null
             )
         },
         title = {
-            Text(text = dialogTitle)
+            Text(
+                text = stringResource(R.string.default_dialog_title),
+                fontWeight = FontWeight.ExtraBold
+            )
         },
         text = {
-            Text(text = dialogText)
+            Text(
+                text = stringResource(R.string.default_dialog_description),
+                fontWeight = FontWeight.ExtraBold
+            )
         },
         onDismissRequest = {
             TODO("On Dismiss Request")
@@ -76,19 +71,19 @@ private fun DefaultDialog(
         confirmButton = {
             TextButton(
                 onClick = {
-                    TODO("Confirm Button")
+                    application.finish()
                 }
             ) {
-                Text("Confirm")
+                Text(stringResource(R.string.dialog_confirm))
             }
         },
         dismissButton = {
             TextButton(
                 onClick = {
-                    TODO("Dismiss Button")
+                    dialogState = false
                 }
             ) {
-                Text("Dismiss")
+                Text(stringResource(R.string.dialog_dismiss))
             }
         }
     )
