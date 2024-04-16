f16d66d71f9347edf03471791023e8f3280a33b1
John Berlin Leonor <johnberlinleonor@yahoo.com>
2024-04-16 21:26:52 +0800

    Add Kdocs for Camera State and VM

--- app/src/main/java/com/ruralnative/handsy/ui/camera_screen/CameraSetupState.kt
+++ app/src/main/java/com/ruralnative/handsy/ui/camera_screen/CameraSetupState.kt
@@ -3,13 +3,13 @@ package com.ruralnative.handsy.ui.camera_screen
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.PermissionState
 
+
 /**
- * Represents the state of the camera permission.
+ * Represents the state of the camera permission within the application.
  *
- * @property cameraPermissionKey An integer key representing the camera permission status.
- * - `0` (default key): Permission is denied and requires no rationale.
- * - `1`: Permission is denied and requires rationale.
- * - `2`: Permission is granted.
+ * @property cameraPermissionState An instance of [PermissionState] that represents
+ * the current state of the camera permission. This state can be used to determine
+ * whether the permission is granted, denied, or if a rationale should be shown to the user.
  */
 data class CameraPermissionState @OptIn(ExperimentalPermissionsApi::class) constructor(
     val cameraPermissionState: PermissionState? = null
--- app/src/main/java/com/ruralnative/handsy/ui/camera_screen/CameraSetupViewModel.kt
+++ app/src/main/java/com/ruralnative/handsy/ui/camera_screen/CameraSetupViewModel.kt
@@ -13,22 +13,54 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 
+/**
+ * ViewModel for managing camera permission state and actions for CameraSetup and its affiliated screens, Rationale and NoRationale.
+ */
 @HiltViewModel
 class CameraPermissionViewModel @Inject constructor() : ViewModel() {
 
     private val _uiState = MutableStateFlow(CameraPermissionState())
     val uiState = _uiState.asStateFlow()
 
+    /**
+     * Updates the camera permission state in the ViewModel.
+     *
+     * This function is used to update the internal state of the camera permission based on the
+     * provided [PermissionState]. The updated state is then emitted to the UI through the [uiState]
+     * flow.
+     *
+     * @param state The new [PermissionState] to set. This can be null, indicating that the
+     * permission state is not yet determined.
+     */
     @OptIn(ExperimentalPermissionsApi::class)
     fun setCameraPermissionState(state: PermissionState?) {
         _uiState.value = _uiState.value.copy(cameraPermissionState = state)
     }
 
+    /**
+     * Returns a lambda function to request camera permission.
+     *
+     * This function generates a lambda that, when invoked, requests the camera permission using
+     * the provided [PermissionState]. The lambda is intended to be used as an onClick handler
+     * for a button or similar UI element.
+     *
+     * @param state The current [PermissionState] that should be used to request the permission.
+     * @return A lambda function that requests the camera permission when invoked.
+     */
     @OptIn(ExperimentalPermissionsApi::class)
     fun requestCameraPermission(state: PermissionState): () -> Unit {
         return { state?.launchPermissionRequest() }
     }
 
+    /**
+     * Opens the application settings screen.
+     *
+     * This function creates an intent to open the application settings screen for the current
+     * application. The settings screen allows the user to manage permissions and other app-specific
+     * settings.
+     *
+     * @param context The [Context] used to start the activity.
+     */
     fun openApplicationSettings(context: Context) {
         val settingsIntent = Intent(
             Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
