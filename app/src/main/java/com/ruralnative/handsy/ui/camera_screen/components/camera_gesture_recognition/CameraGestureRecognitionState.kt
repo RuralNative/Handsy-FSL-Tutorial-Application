33140f0ebacc391d83ddd3d577ebe7ee146bc5f2
John Berlin Leonor <johnberlinleonor@yahoo.com>
2024-04-17 14:26:21 +0800

    Update CameraGestureRecognitionState.kt
    Added properties for the State

--- app/src/main/java/com/ruralnative/handsy/ui/camera_screen/components/camera_gesture_recognition/CameraGestureRecognitionState.kt
+++ app/src/main/java/com/ruralnative/handsy/ui/camera_screen/components/camera_gesture_recognition/CameraGestureRecognitionState.kt
@@ -1,3 +1,10 @@
 package com.ruralnative.handsy.ui.camera_screen.components.camera_gesture_recognition
 
-data class CameraGestureRecognitionState()
+import com.google.mediapipe.tasks.vision.gesturerecognizer.GestureRecognizerResult
+
+data class CameraGestureRecognitionState(
+    val results: List<GestureRecognizerResult>,
+    val inferenceTime: Long = 0,
+    val inputImageHeight: Int = 0,
+    val inputImageWidth: Int = 0
+)
